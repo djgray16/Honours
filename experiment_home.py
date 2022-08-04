e3026798094ce5b28f99411554d69fb9ab8d5de4
djgray16 <djgray16@gmail.com>
2022-08-04 18:25:22 +1000

    changed >= to > for AT agents -
    this fixed the 2 line

--- experiment_home.py
+++ experiment_home.py
@@ -18,17 +18,17 @@ from Models import *
 #import seaborn as sns
 
 import matplotlib.pyplot as plt
-random.seed(2)
+#random.seed(2)
 
 parameters = {
-    'steps': 60, #number of time periods
+    'steps': 40, #number of time periods
     'agent_n': 500,
     'phi':ap.Values(1.2,1.8,2.0,2.2,2.4,2.6,3.0,4.0), #multiplier for common contributions
-    'graph_m' : 6,
+    'graph_m' : 4,
     'graph_alpha': 0.3,
     'graph_p':0.02,
-    'gtype': 'BA',
-    'atype': ReplicatorLocal,
+    'gtype': 'TAG',
+    'atype': AT,
     'plot_G': 0 #gives the summary plot of the graph for each experiment
 }
 
@@ -40,7 +40,7 @@ sample = ap.Sample(
 )
 
 
-exp = ap.Experiment(WealthModel, sample, iterations=20,
+exp = ap.Experiment(WealthModel, sample, iterations=40,
                     record = True)
 results = exp.run()
 
