9192e4de10c6662cce1a39a879fcef7698ee0d9a
djgray16 <djgray16@gmail.com>
2022-09-09 12:24:00 +1000

    yaml

--- comparing_graphs_home.py
+++ comparing_graphs_home.py
@@ -26,10 +26,10 @@ import matplotlib.pyplot as plt
 #### control box
 save = 1 #save the figure
 run =1
-v2 = 'graph_p'
+v2 = 'power_p'
 
 
-filename = 'graph_p_high'
+filename = 'comparing_power_p_low'
 
 title = 'Varying Rewiring p, WS model, Replicator Dynamics'
  #TODO test phi large, then test WS p over phi, then examine cooperation
@@ -42,12 +42,12 @@ parameters = {
     'seed':42,
     'steps': 200, #number of time periods
     'agent_n': 500,
-    'phi':ap.Values(4.5,5.0,5.5,6.0), # #multiplier for common contributions
+    'phi':ap.Values(1.75,2.0,2.25,2.5), # #multiplier for common contributions
     'graph_m' : 6,#ap.Values(4,6,8,10,12),
     'graph_alpha': 0.3,# ap.Values(0.01,0.1,0.25,0.5, 0.75,1.0),
-    'graph_p':ap.Values(0.1,0.2,0.3,0.4,0.5),
-    'power_p': 0.1,#ap.Values(0.1, 0.2,0.3,0.4,0.5),#ap.Values(0.01,0.2,0.4,0.6,0.8),
-    'gtype': 'WS', #ap.Values('WS', 'TAG', 'BA', 'RRG'),
+    'graph_p':0,#ap.Values(0.1,0.2,0.3,0.4,0.5),
+    'power_p': ap.Values(0.1, 0.2,0.3,0.4,0.5),#ap.Values(0.01,0.2,0.4,0.6,0.8),
+    'gtype': 'PL', #ap.Values('WS', 'TAG', 'BA', 'RRG'),
     'atype': ReplicatorLocal,
     'replicator_alpha': 1.0, #1 is pure replicator, 0 is imitation
     'plot_G': 0, #gives the summary plot of the graph for each experiment
